77917fd14ab2243f8f2a21c1724c21ab06a7cd03
pedrinnnnn <pedrocesarsouto@gmail.com>
2023-09-20 22:35:59 -0300

    metodos finais
    metodos implementados sem complemento

--- DataBaseSo/DataBaseSo/KeyValueDatabase.cs
+++ DataBaseSo/DataBaseSo/KeyValueDatabase.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
-namespace DataBaseSo
+namespace BancoDeDadosSO
 {
     public class KeyValueDatabase
     {
@@ -12,7 +13,7 @@ namespace DataBaseSo
             if (!database.ContainsKey(chave))
             {
                 database[chave] = valor;
-                Console.WriteLine(chave,valor);
+                Console.WriteLine(valor);
             }
         }
 
@@ -22,7 +23,7 @@ namespace DataBaseSo
             {
                 database.Remove(chave);
                 Console.WriteLine(chave);
-            }
+            }                     
         }
 
         public void Atualizar(string chave, object novoValor)
@@ -30,15 +31,61 @@ namespace DataBaseSo
             if (database.ContainsKey(chave))
             {
                 database[chave] = novoValor;
-                Console.WriteLine(chave,novoValor);
+                Console.WriteLine(novoValor);
+            }          
+        }
+
+        public object Pesquisar(string chave)
+        {
+            if (database.ContainsKey(chave))
+            {
+                return database[chave];
+            }
+            else
+            {             
+                return null;
             }
         }
 
-        public object Pesquisar()
+        public void SalvarParaArquivo(string nomeArquivo)
         {
-            if (database.ContainsKey())
+            using (StreamWriter writer = new StreamWriter(nomeArquivo))
             {
-                return database[];
+                foreach (var kvp in database)
+                {
+                    writer.WriteLine($"{kvp.Key}:{kvp.Value}");
+                }
+            }
+
+            Console.WriteLine(nomeArquivo);
+        }
+
+        public void CarregarDeArquivo(string nomeArquivo)
+        {
+            if (File.Exists(nomeArquivo))
+            {
+                database.Clear();
+
+                using (StreamReader reader = new StreamReader(nomeArquivo))
+                {
+                    string linha;
+                    while ((linha = reader.ReadLine()) != null)
+                    {
+                        string[] partes = linha.Split(':');
+                        if (partes.Length == 2)
+                        {
+                            string chave = partes[0];
+                            string valor = partes[1];
+                            database[chave] = valor;
+                        }
+                    }
+                }
+
+                Console.WriteLine(nomeArquivo);
+            }
+            else
+            {
+                Console.WriteLine(nomeArquivo);
             }
         }
     }
